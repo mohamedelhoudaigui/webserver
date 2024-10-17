619eccbe332d4115ad1c4ac54f16e3e30265010b
Saif eddine El hanoune <saifeddine13elhanoune@gmail.com>
2024-10-18 00:18:07 +0100

    tired

--- Src/Resp/HttpResponse.cpp
+++ Src/Resp/HttpResponse.cpp
@@ -1,4 +1,6 @@
 #include "../../Include/Response/Response.hpp"
+#include <ctime>
+#include <sstream>
 
 std::string getCurrentTime() {
     time_t now = time(0);
@@ -7,3 +9,56 @@ std::string getCurrentTime() {
     strftime(buffer, 80, "%a, %d %b %Y %H:%M:%S GMT", tm_info);
     return std::string(buffer);
 }
+
+BasicResponse::BasicResponse(int code, const std::string& text) : \
+statusCode(code), statusText(text) {
+    headers["Server"] = "Nginx";
+    headers["Date"] = getCurrentTime();
+}
+
+std::string BasicResponse::getStatusLine() const {
+    std::stringstream ss;
+    ss << "HTTP/1.1" << statusCode << " " << statusText;
+    return ss.str();
+}
+
+std::map<std::string, std::string> BasicResponse::getHeaders() const {
+    return headers;
+}
+
+std::string BasicResponse::getBody() const {
+    return body;
+}
+
+void    BasicResponse::setBody(const std::string& newBody) {
+    std::stringstream ss;
+    body = newBody;
+    ss << body.length();
+    headers["Content-Length"] = ss.str();
+}
+
+ResponseDecorator::ResponseDecorator(Response* res) : response(res) {
+
+};
+
+ResponseDecorator::~ResponseDecorator() {
+    delete response;
+}
+
+std::map<std::string, std::string> ResponseDecorator::getHeaders() const {
+    return response->getHeaders();
+}
+
+std::string  ResponseDecorator::getStatusLine() const {
+    return response->getStatusLine();
+}
+
+std::string ResponseDecorator::getBody() const {
+    return response->getBody();
+}
+
+ContentTypeDecorator::ContentTypeDecorator(Response* res, const std::string& type) :\
+ResponseDecorator(res), contentType(type) {
+
+}
+
